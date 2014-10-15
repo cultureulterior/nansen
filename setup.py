84bfe06a80bd8513c4a2c187396958f395429b37
sam <Samuel Kleiner>
2014-10-15 12:10:22 +0100

    Don't forget setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "nansen",
+    version = "0.0.1",
+    author = "Samuel Kleiner",
+    author_email = "sam@firstbanco.com",
+    description = ("Consul host & docker tree"),
+    license = "GPL",
+    keywords = "consul docker ssh",
+    install_requires = ['npyscreen'],
+    long_description=read('README.md'),
+    scripts=['nansen'],
+    classifiers=[
+        "Topic :: Utilities",
+    ],
+)
